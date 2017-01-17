b09e49a9a5d86e5caa946172d47d8d647e203720
nikolai <nannakoff@hotmail.com>
2017-01-16 20:09:02 -0800

    project file

---
+++ Twittertimer/Twittertimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twittertimer
+//
+//  Created by nikolai on 1/16/17.
+//  Copyright © 2017 Digital Arsenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twittertimer/Twittertimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twittertimer
+//
+//  Created by nikolai on 1/16/17.
+//  Copyright © 2017 Digital Arsenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
